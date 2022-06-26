d1d3e6c20a912d7973c190b77ddb47cf744d6bc8
ajithKweb <107122479+ajithKweb@users.noreply.github.com>
2022-06-26 11:03:24 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'matrimony-demo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
